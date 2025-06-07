e264d561a9fb47b394fa54213c2186e0035f0921
riyaamurdhani <n11921111@qut.edu.au>
2025-06-07 15:09:43 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app'))
 
 from app import create_app, db
 from flask import Flask
